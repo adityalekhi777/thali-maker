2edadffcbb338eec14aa6caf28180cfe37bc1a8c
Aditya Lekhi <adityalekhi777@gmail.com>
2025-01-10 15:36:05 +0530

    Added main components and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Route, Routes } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import Size from './components/Size';
+import Dish from './components/Dish';
+import Order from './components/Order';
 
-export default function App() {
+function App() {
+  const [thali, setThali] = useState({ size: "", dishes: [] });
+
+  const addSize = (size) => {
+    setThali({ ...thali, size })
+  }
+  
+  const addDish = (item) => {
+    let newDishes;
+    if(!thali.dishes.includes(item)){
+      newDishes = [...thali.dishes, item];
+    } else {
+      newDishes = thali.dishes.filter(it => it !== item);
+    }
+    setThali({ ...thali, dishes: newDishes });
+  }
+
   return (
-    <div>App</div>
-  )
+    <>
+      <Header />
+      <Routes>
+        <Route path="/size" element={ <Size addSize={addSize} thali={thali} />}/>
+        <Route path="/dish" element={ <Dish addDish={addDish} thali={thali} />}/>
+        <Route path="/order" element={<Order thali={thali} />}/>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+    </>
+  );
 }
+
+export default App;
